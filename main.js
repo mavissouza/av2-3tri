ce31cc8936e47cfccac2f71f7a9666e8656fa17f
Mavi <106076148+mavissouza@users.noreply.github.com>
2023-11-09 08:16:10 -0300

    mavi

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert( "saudades de jogar um volei no ciro")
+function tocaSom(idElementoAudio) {
+document.querySelectorAll(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onlick = function(){
+        tocaSom ("#som_tecla_aplausos");
+        tocaSom(idAudio)
+    }
+    contador = contador + 1
+    console.log(contador);
+}
